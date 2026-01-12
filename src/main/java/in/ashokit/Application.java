cbf91efddd29562a4e5e2a9ebf497b521fec5f45
Vijay kumar <edigavijay11@gmail.com>
2026-01-12 07:12:29 +0530

    HIS-301:Related changes

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SbWebApplication {
+
+	private Map<String,Object> cache = new HasMap<String,Object>
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		int a=10;
 	}
+	//HIS-301 changes
+	public void loadDataToCache(){
+	   //logic
+        }
 
 }
